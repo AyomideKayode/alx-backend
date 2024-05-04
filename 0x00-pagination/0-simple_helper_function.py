6b1e6bde4fda190c113eed6fedc5e52512c8485f
AyomideKayode <ayomidekay7@gmail.com>
2024-05-04 23:11:56 +0100

    Fix pycodestyle.🚧

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,6 +10,7 @@ Page numbers are 1-indexed, i.e. the first page is page 1.
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple containing the start index and end index for pagination.
