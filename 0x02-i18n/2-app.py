4f475e4d541c396502a92f5532d279b775133dd9
AyomideKayode <ayomidekay7@gmail.com>
2024-05-14 20:22:12 +0100

    Get locale from request.🚧

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 """ Basic Flask app.
-Instantiate the Babel object and configure the app to support multiple languages.
+Instantiate the Babel object and configure the app to support
+multiple languages.
 Create a Config class with the following attributes:
     LANGUAGES (list): List of supported languages.
     BABEL_DEFAULT_LOCALE (str): Default language for the app.
     BABEL_DEFAULT_TIMEZONE (str): Default timezone for the app.
 Define the route for the root URL.
+Create a get_locale function with the babel.localeselector decorator.
+Use request.accept_languages to determine the best match
+with our supported languages.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 # Initialize the Flask application
@@ -17,9 +21,8 @@ app = Flask(__name__)
 # Instantiate the Babel object
 babel = Babel(app)
 
+
 # Create the Config class
-
-
 class Config:
     """
     Configuration class for the application.
@@ -35,11 +38,17 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 # Define the route for the root URL
 @app.route('/')
 def index():
     """Render the index page."""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 # Main entry point of the application
